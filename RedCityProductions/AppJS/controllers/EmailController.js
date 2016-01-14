a2c3449804e6aa65f88a00a24df8149069f7e353
Renzoyzz <renzoyzevallos@gmail.com>
2016-01-14 12:10:52 -0500

    syncing

--- RedCityProductions/AppJS/controllers/EmailController.js
+++ RedCityProductions/AppJS/controllers/EmailController.js
@@ -8,8 +8,46 @@ var RedCityApp;
                 this.emailResource = this.$resource('/api/Email');
             }
             EmailSenderController.prototype.sendEmail = function () {
-                console.log(this.email);
-                this.emailResource.save(this.email);
+                if (this.emailVerification != "8") {
+                    this.failedRequest();
+                    return;
+                }
+                else if (this.email.firstName == "" || this.email.firstName == null) {
+                    this.failedRequest();
+                    return;
+                }
+                else if (this.email.lastName == "" || this.email.lastName == null) {
+                    this.failedRequest();
+                    return;
+                }
+                else if (this.email.phone == "" || this.email.phone == null) {
+                    this.failedRequest();
+                    return;
+                }
+                else if (this.email.emailAddress == "" || this.email.emailAddress == null) {
+                    this.failedRequest();
+                    return;
+                }
+                else if (this.email.message == "" || this.email.message == null) {
+                    this.failedRequest();
+                    return;
+                }
+                else {
+                    try {
+                        this.emailResource.save(this.email);
+                        this.email.firstName = "";
+                        this.email.lastName = "";
+                        this.email.phone = "";
+                        this.email.emailAddress = "";
+                        this.email.message = "";
+                    }
+                    catch (err) {
+                        this.failedRequest();
+                    }
+                }
+            };
+            EmailSenderController.prototype.failedRequest = function () {
+                alert('There was an error sending your message. Please fill all inputs or check if answer is correct');
             };
             return EmailSenderController;
         })();
--- RedCityProductions/AppJS/controllers/EmailController.ts
+++ RedCityProductions/AppJS/controllers/EmailController.ts
@@ -6,6 +6,7 @@
 
         private emailResource 
         private email
+        private emailVerification
 
 
         constructor(private $resource) {
@@ -15,8 +16,49 @@
         }
 
         public sendEmail() {
-            console.log(this.email);
-            this.emailResource.save(this.email);
+            if (this.emailVerification != "8") {
+                this.failedRequest();
+                return;
+            } else if (this.email.firstName == "" || this.email.firstName == null) {
+                this.failedRequest();
+
+                return;
+            } else if (this.email.lastName == "" || this.email.lastName == null) {
+                this.failedRequest();
+
+                return;
+            } else if (this.email.phone == "" || this.email.phone == null) {
+                this.failedRequest();
+
+                return;
+            } else if (this.email.emailAddress == "" || this.email.emailAddress == null) {
+                this.failedRequest();
+
+                return;
+            } else if (this.email.message == "" || this.email.message == null) {
+                this.failedRequest();
+
+                return;
+            } else {
+                try {
+                    this.emailResource.save(this.email);
+                    this.email.firstName = "";
+                    this.email.lastName = "";
+                    this.email.phone = "";
+                    this.email.emailAddress = "";
+                    this.email.message = "";
+
+                } catch (err) {
+                    this.failedRequest();
+                }
+
+            }
+
+        }
+
+        private failedRequest() {
+            alert('There was an error sending your message. Please fill all inputs or check if answer is correct');
+            
         }
 
 
