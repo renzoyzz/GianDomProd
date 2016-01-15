a774cb893c052da958ff2d4c67ac88d132c930dd
Renzo Zevalos <renzoyzevallos@gmail.com>
2016-01-15 14:39:31 -0500

    syncing

--- RedCityProductions/obj/Release/Package/PackageTmp/AppJS/controllers/emailController.js
+++ RedCityProductions/obj/Release/Package/PackageTmp/AppJS/controllers/emailController.js
@@ -8,46 +8,52 @@ var RedCityApp;
                 this.emailResource = this.$resource('/api/Email');
             }
             EmailSenderController.prototype.sendEmail = function () {
+                var _this = this;
                 if (this.emailVerification != "8") {
-                    this.failedRequest();
+                    this.failedRequest('Invalid Answer');
                     return;
                 }
                 else if (this.email.firstName == "" || this.email.firstName == null) {
-                    this.failedRequest();
+                    this.failedRequest("Invalid First Name");
                     return;
                 }
                 else if (this.email.lastName == "" || this.email.lastName == null) {
-                    this.failedRequest();
+                    this.failedRequest("Invalid Last Name");
                     return;
                 }
                 else if (this.email.phone == "" || this.email.phone == null) {
-                    this.failedRequest();
+                    this.failedRequest("Invalid First Name Phone Number");
                     return;
                 }
                 else if (this.email.emailAddress == "" || this.email.emailAddress == null) {
-                    this.failedRequest();
+                    this.failedRequest('Invalid Email Address');
                     return;
                 }
                 else if (this.email.message == "" || this.email.message == null) {
-                    this.failedRequest();
+                    this.failedRequest('Invalid Message');
                     return;
                 }
                 else {
-                    try {
-                        this.emailResource.save(this.email);
-                        this.email.firstName = "";
-                        this.email.lastName = "";
-                        this.email.phone = "";
-                        this.email.emailAddress = "";
-                        this.email.message = "";
-                    }
-                    catch (err) {
-                        this.failedRequest();
-                    }
+                    this.emailResource.save(this.email).$promise.then(function () {
+                        _this.email.firstName = "";
+                        _this.email.lastName = "";
+                        _this.email.phone = "";
+                        _this.email.emailAddress = "";
+                        _this.email.message = "";
+                    }).catch(function (err) {
+                        console.log(err);
+                        var validationErrors = [];
+                        for (var prop in err.data.modelState) {
+                            var propErrors = err.data.modelState[prop];
+                            validationErrors = validationErrors.concat(propErrors);
+                        }
+                        _this.failedRequest(validationErrors);
+                    });
                 }
             };
-            EmailSenderController.prototype.failedRequest = function () {
-                alert('There was an error sending your message. Please fill all inputs or check if answer is correct');
+            EmailSenderController.prototype.failedRequest = function (err) {
+                console.log(err);
+                alert(err);
             };
             return EmailSenderController;
         })();
